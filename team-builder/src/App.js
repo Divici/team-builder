b1d764944c7450b4eb1902beb18f8dc8d84cc183
david-aihe <doa9200@gmail.com>
2021-12-14 13:53:20 -0500

    Changed format of form.js and app.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,19 +3,45 @@ import './App.css';
 import {useState} from 'react';
 import Form from './Form';
 
+const initialFormValues =  {
+  name: '',
+  email: '',
+  role: ''
+  };
+
 function App() {
 
   const [team, setTeam] = useState([]);
+  const [formValues, setFormValues ] = useState(initialFormValues);
 
-  const addTeamMember = (props)=>{
-    const newTeam = team.map(member=> member);
-    newTeam.push({});
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue
+    });
+  }
+
+  const submitForm = () => {
+    const newMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+
+    setTeam(team.concat(newMember));
+    setFormValues(initialFormValues);
   }
 
   return (
     <div className="App">
+      <div>
+        <h1>Our Team</h1>
+      </div>
       {team}
-      <Form />
+      <div>
+        <h2>Add a Member</h2>
+        <Form values={formValues} update={updateForm} submit={submitForm}/>
+      </div>
     </div>
   );
 }
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,39 +1,42 @@
 import React, {useState} from 'react';
 
 const Form = props => {
+    const { values, update, submit } = props
   
-  const [formData, setFormData] = useState({
-      name: '',
-      email: '',
-      role: ''
-  });
+    const onInputChange = event =>{
+        const name = event.target.name;
+        const {value} = event.target;
 
-  const onInputChange = event =>{
-    setFormData({
-        ...formData,
-        [event.target.name]: event.target.value,
-    });
-  };
+        update(name, value);
+    };
+
+  const onSubmit = evt =>{
+    evt.preventDefault();
+    submit();
+  }
 
   return (
     <div>
-        <form>
+        <form onSubmit={onSubmit}>
             <label>
-                Name: <input name='name' onChange={onInputChange} type='text' />
+                Name: <input name='name' value={values.name} placeholder='Type your username here' maxLength='30' onChange={onInputChange} type='text' />
             </label>
             <label>
-                Email: <input name='email' onChange={onInputChange} type='text' />
+                Email: <input name='email' value={values.email} placeholder='Type your email here' maxLength='30' onChange={onInputChange} type='text' />
             </label>
             <label>
-                Role: <select name='role'onChange={onInputChange} >
-                    <option />
-                    <option>Fullstack Developer</option>
-                    <option>Frontend Developer</option>
-                    <option>Backend Developer</option>
-                    <option>UI/UX Designer</option>
-                    <option>Project Manager</option>
+                Role: <select name='role' onChange={onInputChange} >
+                    <option value=''>--Select a Role--</option>
+                    <option value='fullstack'>Fullstack Developer</option>
+                    <option value='frontend'>Frontend Developer</option>
+                    <option value='backend'>Backend Developer</option>
+                    <option value='uiux'>UI/UX Designer</option>
+                    <option value='projectManager'>Project Manager</option>
                 </select>
             </label>
+            <div>
+                <button>submit</button>
+            </div>
         </form>
     </div>
   );
