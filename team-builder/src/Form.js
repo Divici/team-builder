2142ab6904c6cca3503e75ce49d85dfe0cf7bebb
david-aihe <doa9200@gmail.com>
2021-12-14 11:33:10 -0500

    Added select form to form.js and rendered form to app

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -12,7 +12,25 @@ const Form = props => {
                 Name: <input onChange={event => {
                     setName(event.target.value);
                 }} 
-                id='Name' type='text' />
+                id='name' type='text' />
+            </label>
+            <label>
+                Email: <input onChange={event => {
+                    setEmail(event.target.value);
+                }} 
+                id='email' type='text' />
+            </label>
+            <label>
+                Role: <select onChange={event => {
+                    setRole(event.target.value);
+                }} id='role' >
+                    <option />
+                    <option>Fullstack Developer</option>
+                    <option>Frontend Developer</option>
+                    <option>Backend Developer</option>
+                    <option>UI/UX Designer</option>
+                    <option>Project Manager</option>
+                </select>
             </label>
         </form>
     </div>
